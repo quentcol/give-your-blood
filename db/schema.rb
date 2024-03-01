4be7ab2739cee39f30a42c1b0450df7cf5709b67
quentcol <quent.col@gmail.com>
2024-03-01 14:12:11 +0100

    I have created the appointment model and linked it to both patient and center

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_130434) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_131115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.string "person_in_charge"
+    t.bigint "patient_id", null: false
+    t.bigint "center_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["center_id"], name: "index_appointments_on_center_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "centers", force: :cascade do |t|
     t.string "address"
@@ -42,4 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_130434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "centers"
+  add_foreign_key "appointments", "patients"
 end
