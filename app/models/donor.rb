ddc73cc04a3f013a371c7850f792d738983a0eb7
mahmoud moursy <mahmoud.salah.rabee@gmail.com>
2024-03-08 12:59:03 +0100

    fixed new appointment and new schedule

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,5 +2,9 @@ class Donor < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :appointments
-  validates :user_id, uniqueness: true 
+  validates :user_id, uniqueness: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,4 @@
 class Schedule < ApplicationRecord
   belongs_to :day
   belongs_to :center
-  validates :day_id, :center_id, :opening_time, :closing_time, presence: true
 end
