1d38983a8bd11d96d35b2575c03521d485ee676d
iheb laib <bba3at@gmail.com>
2024-03-16 11:37:05 +0000

    added the Reviews in the Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-
+Review.delete_all
 Appointment.delete_all
 Schedule.delete_all
 Center.delete_all
@@ -10,17 +10,28 @@ Donor.delete_all
 User.delete_all
 Day.delete_all
 
+
 puts "creating users"
-user_1 = User.create!({ email: 'host-a@host.com', password: 'password123', category: 'donor' })
-user_2 = User.create!({ email: 'host-b1@host.com', password: 'password12', category: 'donor' })
-user_3 = User.create!({ email: 'host-b2@host.com', password: 'password1234', category: 'hospital' })
+user_1 = User.create!({ email: 'host-a@host.com', password: 'password123', category: 'hospital' })
+user_2 = User.create!({ email: 'host-b1@host.com', password: 'password12', category: 'hospital' })
+user_3 = User.create!({ email: 'host-b2@host.com', password: 'password1234', category: 'donor' })
+user_4 = User.create!({ email: 'test@gmail.com', password: 'test123', category: 'donor' })
+user_5 = User.create!({ email: 'user1@example.com', password: 'user123', category: 'hospital' })
+user_6 = User.create!({ email: 'user2@example.com', password: 'user456', category: 'donor' })
+user_7 = User.create!({ email: 'user3@example.com', password: 'user789', category: 'donor' })
+user_8 = User.create!({ email: 'user4@example.com', password: 'user000', category: 'hospital' })
 
 puts "creating donors"
 donor_1 = { user: user_1, first_name: 'Iheb', last_name: "Laib", blood_type: 'A+', address: 'New York', birthdate: '1995-01-01' }
 donor_2 = { user: user_2, first_name: 'Mahmoud', last_name: "Moursay", blood_type: 'B+', address: 'Los Angeles', birthdate: '1990-01-01' }
 donor_3 = { user: user_3, first_name: 'Robin', last_name: "Gerritsen", blood_type: 'O+', address: 'Chicago', birthdate: '1992-01-01' }
+donor_4 = { user: user_4, first_name: 'John', last_name: "Aren", blood_type: 'O+', address: 'Washington', birthdate: '1993-01-01' }
+donor_5 = { user: user_5, first_name: 'Linda', last_name: "Smith", blood_type: 'AB-', address: 'Amsterdam', birthdate: '1988-05-15' }
+donor_6 = { user: user_6, first_name: 'Emma', last_name: "Johnson", blood_type: 'O-', address: 'Rotterdam', birthdate: '1991-08-20' }
+donor_7 = { user: user_7, first_name: 'Tom', last_name: "Wilson", blood_type: 'A-', address: 'The Hague', birthdate: '1985-11-10' }
+donor_8 = { user: user_8, first_name: 'Sophia', last_name: "Brown", blood_type: 'B-', address: 'Utrecht', birthdate: '1997-03-25' }
 
-[donor_1, donor_2, donor_3].each do |info|
+[donor_1, donor_2, donor_3, donor_4, donor_5, donor_6, donor_7, donor_8].each do |info|
   donor = Donor.create!(info)
   puts "created #{donor.first_name}"
 end
@@ -29,28 +40,56 @@ puts "creating hospitals"
 hospital_1 = Hospital.create!({ user: user_1, hospital_name: 'General Hospital' })
 hospital_2 = Hospital.create!({ user: user_2, hospital_name: 'Children Hospital' })
 hospital_3 = Hospital.create!({ user: user_3, hospital_name: 'Veterans Hospital' })
+hospital_4 = Hospital.create!({ user: user_4, hospital_name: 'Kingston Hospital' })
+hospital_5 = Hospital.create!({ user: user_5, hospital_name: 'City Hospital' })
+hospital_6 = Hospital.create!({ user: user_6, hospital_name: 'University Hospital' })
+hospital_7 = Hospital.create!({ user: user_7, hospital_name: 'Saint Mary Hospital' })
+hospital_8 = Hospital.create!({ user: user_8, hospital_name: 'Royal Hospital' })
 
 puts "creating centers"
-center_1 = Center.create!({ hospital: hospital_1, name: 'Blood Center 1', address: 'New York' })
-center_2 = Center.create!({ hospital: hospital_2, name: 'Blood Center 2', address: 'Los Angeles' })
-center_3 = Center.create!({ hospital: hospital_3, name: 'Blood Center 3', address: 'Chicago' })
+center_1 = Center.create!({ hospital: hospital_1, name: 'Blood Center 1', address: 'Leidschedam' })
+center_2 = Center.create!({ hospital: hospital_2, name: 'Blood Center 2', address: 'Schiedam' })
+center_3 = Center.create!({ hospital: hospital_3, name: 'Blood Center 3', address: 'Wateringen' })
+center_4 = Center.create!({ hospital: hospital_4, name: 'Blood Center 4', address: 'Delft' })
+center_5 = Center.create!({ hospital: hospital_5, name: 'Blood Center 5', address: 'Amsterdam' })
+center_6 = Center.create!({ hospital: hospital_6, name: 'Blood Center 6', address: 'Rotterdam' })
+center_7 = Center.create!({ hospital: hospital_7, name: 'Blood Center 7', address: 'The Hague' })
+center_8 = Center.create!({ hospital: hospital_8, name: 'Blood Center 8', address: 'Utrecht' })
 
 puts "creating days"
-weekdays = %w[Monday Tuesday Wednesday Thursday Friday Saturday]
-
+weekdays = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
 weekdays.each do |day_name|
-
   Day.create!(name: day_name)
 end
 
 puts "creating schedules"
-schedules_1 = Schedule.create!({ center_id: Center.first.id, day: Day.first, opening_time: '09:00', closing_time: '12:00' })
-schedules_2 = Schedule.create!({ center_id: Center.second.id, day: Day.second, opening_time: '09:00', closing_time: '12:00' })
-schedules_3 = Schedule.create!({ center_id: Center.third.id, day: Day.third, opening_time: '09:00', closing_time: '12:00' })
+schedules_1 = Schedule.create!({ center_id: center_1.id, day: Day.first, opening_time: '09:00', closing_time: '12:00' })
+schedules_2 = Schedule.create!({ center_id: center_2.id, day: Day.second, opening_time: '09:00', closing_time: '12:00' })
+schedules_3 = Schedule.create!({ center_id: center_3.id, day: Day.third, opening_time: '09:00', closing_time: '12:00' })
+schedules_4 = Schedule.create!({ center_id: center_4.id, day: Day.fourth, opening_time: '09:00', closing_time: '12:00' })
+schedules_5 = Schedule.create!({ center_id: center_5.id, day: Day.fifth, opening_time: '09:00', closing_time: '12:00' })
+schedules_6 = Schedule.create!({ center_id: center_6.id, day: Day.fifth, opening_time: '09:00', closing_time: '12:00' })
+schedules_7 = Schedule.create!({ center_id: center_7.id, day: Day.third, opening_time: '09:00', closing_time: '12:00' })
+schedules_8 = Schedule.create!({ center_id: center_8.id, day: Day.first, opening_time: '09:00', closing_time: '12:00' })
 
 puts "creating appointments"
 appointment_1 = Appointment.create!({ donor_id: Donor.first.id, center_id: center_1.id })
 appointment_2 = Appointment.create!({ donor_id: Donor.second.id, center_id: center_2.id })
 appointment_3 = Appointment.create!({ donor_id: Donor.third.id, center_id: center_3.id })
+appointment_4 = Appointment.create!({ donor_id: Donor.fourth.id, center_id: center_4.id })
+appointment_5 = Appointment.create!({ donor_id: Donor.fifth.id, center_id: center_5.id })
+appointment_6 = Appointment.create!({ donor_id: Donor.fifth.id, center_id: center_6.id })
+appointment_7 = Appointment.create!({ donor_id: Donor.third.id, center_id: center_7.id })
+appointment_8 = Appointment.create!({ donor_id: Donor.first.id, center_id: center_8.id })
+
+puts "creating reviews"
+review_1 = Review.create!({ donor: Donor.first, center_id: center_1.id, rating: 4, review_content: "Great experience!" })
+review_2 = Review.create!({ donor: Donor.second, center_id: center_2.id, rating: 5, review_content: "Highly recommended!"})
+review_3 = Review.create!({ donor: Donor.third, center_id: center_3.id, rating: 3, review_content: "Easy going Nurse." })
+review_4 = Review.create!({ donor: Donor.fourth, center_id: center_4.id, rating: 2, review_content: "long waiting List" })
+review_5 = Review.create!({ donor: Donor.fifth, center_id: center_5.id, rating: 4, review_content: "Nice facilities." })
+review_6 = Review.create!({ donor: Donor.fifth, center_id: center_6.id, rating: 5, review_content: "Close to my House" })
+review_7 = Review.create!({ donor: Donor.third, center_id: center_7.id, rating: 3, review_content: "Freindly Staff!" })
+review_8 = Review.create!({ donor: Donor.first, center_id: center_8.id, rating: 4, review_content: "Quick service!" })
 
 puts "seeds done!"
