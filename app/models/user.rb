506e933afdd041f80202877f29ea67cb9732b927
quentcol <quent.col@gmail.com>
2024-03-19 19:23:15 +0100

    I have excluded admin from mass assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   enum category: { donor: 0, hospital: 1 }
   validates :category, presence: true, if: :new_record?
   has_many :hospitals
-  has_one :donor 
+  has_one :donor
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessible :email, :password, :password_confirmation
 
 end
