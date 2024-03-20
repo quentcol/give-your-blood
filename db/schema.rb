882c436fc98fc705c5c2cf5f90586f315ac01ba9
quentcol <quent.col@gmail.com>
2024-03-20 11:30:24 +0100

    I have removed is_admin from the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_181853) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_102618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,7 +96,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_181853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category"
-    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
