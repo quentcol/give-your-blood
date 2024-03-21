40ce755e72e429b72a22a59503ceb555f3d712c6
quentcol <quent.col@gmail.com>
2024-03-21 15:51:44 +0100

    I have defined the enum status in my appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :donor
   belongs_to :center
 
+  enum status: { pending: 0, approved: 1, denied: 2, canceled: 3 }
+
   def mark_as_canceled?
     update(status: 'canceled')
   end
