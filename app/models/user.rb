f6261ec3358d7f111c4dc95032ef1774f15bf443
mahmoud moursy <mahmoud.salah.rabee@gmail.com>
2024-03-22 07:20:08 +0100

    fixing indexmap plus adding the create profile if sign up as a hospital

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   enum category: { donor: 0, hospital: 1 }
   validates :category, presence: true, if: :new_record?
-  has_many :hospitals
+  has_one :hospital
   has_one :donor
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
